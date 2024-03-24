71bf9186814792a0ca7969b1501e4d74b1351a83
H3rmel <isaachermel@gmail.com>
2024-03-24 14:17:02 -0300

    fix: improve context verify por theme provider

--- src/ui/components/theme/theme-provider.tsx
+++ src/ui/components/theme/theme-provider.tsx
@@ -76,7 +76,7 @@ export function ThemeProvider({
 export const useTheme = (): ThemeProviderState => {
   const context = useContext(ThemeProviderContext);
 
-  if (context === undefined) throw new Error('useTheme must be used within a ThemeProvider');
+  if (!context) throw new Error('useTheme must be used within a ThemeProvider');
 
   return context;
 };
