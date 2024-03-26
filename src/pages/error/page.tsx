4c4f09da5255c098a1cc8c24641f4857f43f20c8
H3rmel <isaachermel@gmail.com>
2024-03-25 23:48:33 -0300

    docs: todo comment added

--- src/pages/error/page.tsx
+++ src/pages/error/page.tsx
@@ -2,7 +2,7 @@ import { useRouteError } from "react-router-dom";
 
 export function ErrorPage() {
   const error = useRouteError();
-
+  // TODO: Create the ErrorBoundary instead of ErrorPage (needs to be in the /components/ directory)
   console.log(error);
 
   return <>error</>;
