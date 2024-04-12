3fa51f89a0e50c764bacb403a958b5e6304ad05b
Isaac Hermel <97918507+h3rmel@users.noreply.github.com>
2024-04-12 19:50:18 -0300

    fix: added mapSelectOption util to fix unexpected behavior of select option (#18)

--- src/types/px-rem.d.ts
+++ src/types/px-rem.d.ts
@@ -1,3 +1,5 @@
 type PxRemPaddingType = 'p' | 'px' | 'py' | 'ps' | 'pe' | 'pt' | 'pr' | 'pb' | 'pl';
 
 type PxRemMarginType = 'm' | 'mx' | 'my' | 'ms' | 'me' | 'mt' | 'mr' | 'mb' | 'ml';
+
+type PxRemSelectOption = 'gap' | PxRemPaddingType | PxRemMarginType;
