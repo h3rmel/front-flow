ca52ea64b87de4d2e7d42d3b20dee40e5e79d859
H3rmel <isaachermel@gmail.com>
2024-04-12 11:36:25 -0300

    feat: more options to info-table component

--- src/app/tools/px-rem/page.tsx
+++ src/app/tools/px-rem/page.tsx
@@ -4,14 +4,9 @@
 
 import { ArrowsHorizontal } from '@phosphor-icons/react';
 
-import { PxRemForm, InfoTable, PX_REM_CONVERSION, PX_REM_LANGUAGES } from '@/features/px-rem';
-
-import { useLanguage } from '@/ui/components/language/language-provider';
+import { PxRemForm, ConversionTable } from '@/features/px-rem';
 
 // #endregion
-
-const FIRST_HALF = PX_REM_CONVERSION.slice(0, PX_REM_CONVERSION.length / 2);
-const SECOND_HALF = PX_REM_CONVERSION.slice(PX_REM_CONVERSION.length / 2, PX_REM_CONVERSION.length);
 
 // TODO: Work on a way to export metadata. (remove the use client from the top.)
 // export const metadata: Metadata = {}
@@ -22,8 +17,6 @@ const SECOND_HALF = PX_REM_CONVERSION.slice(PX_REM_CONVERSION.length / 2, PX_REM
  * @return {JSX.Element} The content of the PX to REM page.
  */
 export default function Page(): JSX.Element {
-  const { translate } = useLanguage();
-
   return (
     <main>
       <section className="relative container flex flex-col items-center justify-center gap-8 p-16">
@@ -34,11 +27,7 @@ export default function Page(): JSX.Element {
         {/* PX <-> Rem form */}
         <PxRemForm />
         {/* Conversion Tables */}
-        <h2 className="text-2xl">{translate('conversion_table', PX_REM_LANGUAGES)}</h2>
-        <div className="w-full flex items-center justify-around">
-          <InfoTable tableContent={FIRST_HALF} />
-          <InfoTable tableContent={SECOND_HALF} />
-        </div>
+        <ConversionTable />
       </section>
     </main>
   );
---
+++ src/types/px-rem.d.ts
@@ -0,0 +1,3 @@
+type PxRemPaddingType = 'p' | 'px' | 'py' | 'ps' | 'pe' | 'pt' | 'pr' | 'pb' | 'pl';
+
+type PxRemMarginType = 'm' | 'mx' | 'my' | 'ms' | 'me' | 'mt' | 'mr' | 'mb' | 'ml';
